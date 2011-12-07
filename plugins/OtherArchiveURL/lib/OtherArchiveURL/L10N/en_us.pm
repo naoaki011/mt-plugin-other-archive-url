6515496f9c28a98160b17511d431f3d7ebef1fd7
naoaki011 <naoaki@orion.ocn.ne.jp>
2011-12-07 16:23:44 +0900

    add l10n.

---
+++ plugins/OtherArchiveURL/lib/OtherArchiveURL/L10N/en_us.pm
@@ -0,0 +1,9 @@
+package OtherArchiveURL::L10N::en_us;
+
+use strict;
+use base 'OtherArchiveURL::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    );
+1;
---
+++ plugins/OtherArchiveURL/lib/OtherArchiveURL/L10N/ja.pm
@@ -0,0 +1,10 @@
+package OtherArchiveURL::L10N::ja;
+
+use strict;
+use base 'OtherArchiveURL::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Linking to Non-Preferred Archives.' => '優先アーカイブ以外のアーカイブにリンクします。',
+    );
+1;
